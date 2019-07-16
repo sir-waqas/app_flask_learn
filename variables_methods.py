576b0a442575bcd8b810096572bd57df25d4d9ba
Waqas Azam <waqas@MBPRO-WAQASAZAM.local>
2019-07-16 12:55:04 +0500

    Vars and Methods

---
+++ variables_methods.py
@@ -0,0 +1,15 @@
+# Variables
+a = 5
+print(a)
+# Excercise
+var1 = var2 = 4
+print(var1)
+print(var2)
+num1 = num2 = 4
+print(num1*num2)
+
+# Methods
+
+def custom_print(argument):
+    print(argument)
+custom_print("Something")
